7833026f4a43f601bcd8a2d7050b57c78217f211
Raul Reis <raul.soares@usp.br>
2024-02-01 20:08:11 -0300

    Criado o conteúdo da recycler view com uma requesição HTTP e criado uma toolbar com a logo e um menu

--- app/src/main/java/br/com/raulreis/videotube/MainActivity.kt
+++ app/src/main/java/br/com/raulreis/videotube/MainActivity.kt
@@ -1,22 +1,96 @@
 package br.com.raulreis.videotube
 
+import android.content.res.ColorStateList
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.raulreis.videotube.databinding.ActivityMainBinding
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var videoAdapter: VideoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = ""
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            when(resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    binding.imgBackLogo.imageTintList = ColorStateList.valueOf(Color.WHITE)
+                }
+            }
+        }
+
+        val videos = mutableListOf<Video>()
+        videoAdapter = VideoAdapter(videos) {video: Video ->
+
+        }
+
         with(binding) {
             rvMain.layoutManager = LinearLayoutManager(this@MainActivity)
-            rvMain.adapter = VideoAdapter()
+            rvMain.adapter = videoAdapter
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val res = async { getVideos() }
+            val listVideos = res.await()
+            withContext(Dispatchers.Main) {
+                listVideos?.let {
+                    videos.clear()
+                    videos.addAll(listVideos.data)
+                    videoAdapter.notifyDataSetChanged()
+                    binding.FrameProgress.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun getVideos(): ListVideo? {
+        val client = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .get()
+            .url("https://tiagoaguiar.co/api/youtube-videos")
+            .build()
+
+        return try {
+            val response = client.newCall(request).execute()
+
+            if (response.isSuccessful) {
+                GsonBuilder().create()
+                    .fromJson(response.body()?.string(), ListVideo::class.java)
+            }
+            else {
+                null
+            }
+        } catch (e: Exception) {
+            null
         }
     }
 }
--- app/src/main/java/br/com/raulreis/videotube/VideoAdapter.kt
+++ app/src/main/java/br/com/raulreis/videotube/VideoAdapter.kt
@@ -3,25 +3,40 @@ package br.com.raulreis.videotube
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
-class VideoAdapter() : RecyclerView.Adapter<VideoAdapter.VideoHolder>() {
+class VideoAdapter(private val videos : List<Video>, val onClick: (Video) -> Unit) : RecyclerView.Adapter<VideoAdapter.VideoHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoHolder {
         return VideoHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_video, parent, false))
     }
 
     override fun onBindViewHolder(holder: VideoHolder, position: Int) {
-        holder.bind()
+        holder.bind(videos[position])
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return videos.size
     }
 
     inner class VideoHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind() {
+        fun bind(video: Video) {
+            with(itemView) {
+                setOnClickListener {
+                    onClick(video)
+                }
+                Picasso.get().load(video.thumbnailUrl).into(findViewById<ImageView>(R.id.imgThumbnail))
+                Picasso.get().load(video.publisher.pictureProfileUrl).into(findViewById<CircleImageView>(R.id.imgAuthor))
+                findViewById<TextView>(R.id.txvTitle).text = video.title
+                findViewById<TextView>(R.id.txvInfo).text = context.getString(R.string.info,
+                        video.publisher.name, video.viewsCountLabel, video.publishedAt.formatted()
+                    )
 
+            }
         }
     }
 }
